d993c28e335292e0e9883c559858e7a8bb4daf3d
msisengali@gmail.com <msisengali@gmail.com>
2017-07-26 02:54:24 +0600

    ffff

--- src/emails.py
+++ src/emails.py
@@ -1,4 +1,22 @@
 # email related methods here
 
 def send_email(email_address, message):
-	pass
+	import smtplib
+	
+	FROM = 'msisengali@gmail.com' 
+	TO = [email_address]
+	SUBJECT = "Notification"
+	TEXT = message
+	message = """\
+	From: %s
+	To: %s
+	Subject: %s
+
+	%s
+	""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.ehlo()
+	server.starttls()
+	server.login('msisengali@gmail.com','margosha777')
+	server.sendmail(FROM, TO, message)
+	server.quit()
