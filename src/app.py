d044c4f792912c79b629364063a335c14d55a152
Allison <allisonnicoledeal@gmail.com>
2017-07-25 02:17:52 -0700

    add db part

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
 from flask import Flask
+
+import calc
+import db
+import email
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
@@ -21,27 +26,28 @@ def getVotingPage():
 
 @app.route('/submitVote', methods=['POST'])
 def submitVote(email, location, start_date, end_date, price_min, price_max, has_car=None, has_cleaning=None, has_fitness=None,
-       has_wifi=None, has_attractions=None, has_restaurant=None, has_spa=None, has_pool=None, has_view=None, is_hotel=None,
-       is_bnb=None, is_villa=None, is_apt=None, is_campsite=None, is_resort=None):
-    db.store_vote_res(email, location, start_date, end_date, price_min, price_max, has_car=None, has_cleaning=None, has_fitness=None,
-       has_wifi=None, has_attractions=None, has_restaurant=None, has_spa=None, has_pool=None, has_view=None, is_hotel=None,
-       is_bnb=None, is_villa=None, is_apt=None, is_campsite=None, is_resort=None)
+               has_wifi=None, has_attractions=None, has_restaurant=None, has_spa=None, has_pool=None, has_view=None,
+               is_hotel=None, is_bnb=None, is_villa=None, is_apt=None, is_campsite=None, is_resort=None):
     # store to trip details db
+    db.store_vote_result(email, location, start_date, end_date, price_min, price_max, has_car=None, has_cleaning=None,
+                         has_fitness=None, has_wifi=None, has_attractions=None, has_restaurant=None, has_spa=None, has_pool=None,
+                         has_view=None, is_hotel=None, is_bnb=None, is_villa=None, is_apt=None, is_campsite=None, is_resort=None)
     return 'Rebellama'
 
 # this will be called by a cron job
 # cron job will scan db for voting complete, but stats missing
 @app.route('/getBestHotels', methods=['GET'])
-def getBestHotels(limit):
+def getBestHotels(trip_id, limit):
     # calculate best pct and find best hotels, store to db
+    calc.get_best_hotels(trip_id, limit)
     # notify organizer
+    email.send_vote_results()
     return 'Rebellama'
 
 @app.route('/bookRoom', methods=['POST'])
 def bookRoom(limit):
-    # use booking API to book room
+    # mock this portion for now
     return 'Rebellama'
-
 
 
 if __name__ == '__main__':
--- src/db.py
+++ src/db.py
@@ -1,1 +1,48 @@
 #db methods here
+import sqlalchemy
+# import MySQLdb
+
+from cfg import DB_STR
+
+
+# db = sqlalchemy.create_engine(cfg.DB_STR, pool_recycle=600, pool_size=16)
+class TripDB(object):
+
+	def __init__(self):
+		# self.conn = self.get_conn(DB['host'], user=DB['user'], passwd=DB['pw'], db=DB['database'])
+		self.db_pool = sqlalchemy.create_engine(DB_STR)
+
+	def get_conn(self):
+		self.db_pool.connect()
+
+    def fetchall(self, query):
+        conn = self.get_conn()
+        result = [dict(u) for u in conn.execute(query).fetchall()]
+        conn.close()
+        return result
+
+    def fetchone(self, query):
+        conn = self.get_conn()
+        r = conn.execute(query).fetchone()
+        result = None
+        if r:
+            result = dict(r)
+        conn.close()
+        return result
+
+
+    def insert(self, query):
+        conn = self.get_conn()
+
+
+
+    def store_vote_result(self, email, location, start_date, end_date, price_min, price_max, has_car=None, has_cleaning=None,
+                          has_fitness=None, has_wifi=None, has_attractions=None, has_restaurant=None, has_spa=None, has_pool=None,
+                          has_view=None, is_hotel=None, is_bnb=None, is_villa=None, is_apt=None, is_campsite=None, is_resort=None):
+    	query = 
+
+
+
+if __name__ == "__main__":
+	trip_db = TripDB()
+
